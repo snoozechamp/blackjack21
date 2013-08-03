9728a1153f2b270c3c25fa5728dbbbaadc9f681a
Umair Naveed <unaveed@gmail.com>
2013-08-02 23:34:54 -0600

    First commit

---
+++ Blackjack21/src/blackjack/Cards.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+public class Cards {
+
+	// This will be the class representing card objects
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
