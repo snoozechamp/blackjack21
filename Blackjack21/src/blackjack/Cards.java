83fb2ccff8d12fc058054278f33aaa3696eee730
Umair Naveed <unaveed@gmail.com>
2013-08-06 00:15:51 -0600

    Changed the Card class to only have card information. Added a getter method to display only the value of the card.

--- Blackjack21/src/blackjack/Cards.java
+++ Blackjack21/src/blackjack/Cards.java
@@ -1,49 +1,37 @@
 package blackjack;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Cards {
 
 	private int value;		// Holds the face value of each card.
 	private String name;	// Holds the name of the card, i.e. King, Queen, etc.
-	private String suit;	// Holds the name of the suit.
-	static ArrayList<Cards> deck;
+	private String suit;	// Holds the name of the suit.	
 	
-	 // All the possible card names
-	static String[] cards = new String[]{"2", "3", "4", "5", "6", "7",
-								  "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-	
-	// All the possible suits
-	static String[] suits = new String[]{"Clubs", "Diamonds", "Hearts", "Spades"};		
-	
-	// This will be the class representing card objects
+	// Represents each individual card
 	public Cards (int i, String name, String s) {
 		this.value = i;
 		this.name = name;
 		this.suit = s;
 	}
+
+	/**
+	 * Getter that allows easy displaying of card data.
+	 * @return String with information about cards.
+	 */
+	public String get(){
+		return "Your card is: \nsuit-" + 
+				this.suit +" \nname-" + 
+				this.name + " \nvalue-" + 
+				this.value;
+	}
 	
 	/**
-	 * @param args
+	 * Specific getter, retrieves the value of the card.
+	 * @return - Integer value of the card.
 	 */
-	public static void main(String[] args) {
-		deck = new ArrayList<Cards>();
-		for(int i = 0; i < suits.length; i++){
-			for(int j = 0; j < cards.length; j++){
-				int k = 2;
-				deck.add(new Cards(k, cards[j], suits[i]));
-				k++;
-			}
-		}
-		System.out.println(deck.get(5).get());
-
-	}
-	public static void build (){
-
-	}
-	public String get(){
-		return "Your card is a " + this.suit +" with this value " + this.name;
-		
+	public int getValue(){
+		return this.value;
 	}
 
 }
