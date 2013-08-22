5225e6b98349c445df0a5741eb908764f1e6230a
snoozechamp <unaveed@gmail.com>
2013-08-21 23:19:13 -0600

    Update Game.java
    Catching up the master branch with the head branch

--- Blackjack21/src/blackjack/Game.java
+++ Blackjack21/src/blackjack/Game.java
@@ -2,23 +2,34 @@ package blackjack;
 
 import java.util.*;
 import javax.swing.JOptionPane;
-
+/**
+ * @author Umair Naveed
+ */
+/**TODO - Add the ability to split. 
+ * Hide the dealers card
+ * @author umairnaveed
+ *
+ */
 public class Game {
 	
-	private boolean state;			// State of the game, if false game over
-	private boolean turn;			// Determines whose turn it is, true if player's turn
-	public double money;			// Represents the amount of money the player has
-	private ArrayList<Cards> deck;	// Represents a deck of 52 playing cards
-	private String[] suits;			// Represents each suit in a deck of cards
-	private String[] cards;			// Represents each type of card in a deck
-	public ArrayList<Cards> player; // Represents the cards that the player has been dealt
-	public ArrayList<Cards> dealer; // Represents the cards that the dealer has been dealt
+	private int state;					// Represents the state of the game and determines who wins the round
+	private boolean turn;				// Determines whose turn it is, true if player's turn
+	private boolean hitDoubleStand;		// Determines players choice to hit, double, or stand
+	private double money;				// Represents the amount of money the player has
+	private double bet;					// Represents the player's bet
+	private ArrayList<Cards> deck;		// Represents a deck of 52 playing cards
+	private String[] suits;				// Represents each suit in a deck of cards
+	private String[] cards;				// Represents each type of card in a deck
+	private ArrayList<Cards> player; 	// Represents the cards that the player has been dealt
+	private ArrayList<Cards> dealer; 	// Represents the cards that the dealer has been dealt
+	private int size;
 
 
 	public Game (int amount){
 		// Initialize all the variables
-		state = true;
+		state = 0;
 		money = amount;
+		hitDoubleStand = true;
 		player = new ArrayList<Cards>();
 		dealer = new ArrayList<Cards>();
 		deck = new ArrayList<Cards>();
@@ -28,33 +39,75 @@ public class Game {
 		
 		// Builds deck of cards
 		build(deck, suits, cards);
+		size = deck.size();
 		
 		// Deals two cards to the dealer and two to the player
+		String sBet = JOptionPane.showInputDialog("How much would you like to bet?");
+		double bet = Double.parseDouble(sBet);
 		for(int i = 0; i < 4; i++){
-			deal(deck.size(), turn);
+			deal(size, turn);
 			if(turn)
 				turn = false;
 			else 
 				turn = true;
 		}
-		if(score(player) > 21){
-			state = false;
-		}
-		if(score(dealer) > 21){
-			state = false;
-		}
+		System.out.println("Player score: " + score(player));
+		System.out.println("Dealer score: " + score(dealer));
+
+		int initial = 0;
 		
-		if(state){
-			String choose = JOptionPane.showInputDialog("Would you like to hit that?");
-			if(choose.equalsIgnoreCase("yes")){
-				System.out.println(score(player));
-				deal(deck.size(), true);
+		// The player loop
+		while(state == 0){
+			String choose;
+			if(initial == 0)
+				choose = JOptionPane.showInputDialog("Would you like to hit, double, or stand?");
+			
+			else 
+				choose = JOptionPane.showInputDialog("Would you like to hit or stand?");
+
+			// If the player chooses to hit, they receive another card, hitDoubleStand allows
+			// them to have the option to hit again provided they haven't busted.
+			if(choose.equalsIgnoreCase("hit") && hitDoubleStand){
+				hitDoubleStand = true;
+				deal(size, true);
 				System.out.println("New score: " + score(player) + " and card drawn: " + player.get(player.size() - 1).getName());
 				turn = false;
-				System.out.println(score(dealer));
+			}
+			// Only available after the first two cards are dealt, the player is allowed
+			// to double their bet and receive only one card. After which, they are forced
+			// stand on this card. 
+			else if (choose.equalsIgnoreCase("double") && initial == 0){
+				hitDoubleStand = false;
+				bet *= 2;
+				deal(size, true);
+			}
+			// Receive no more cards and determine the score. 
+			else {
+				hitDoubleStand = false;
+			}
+			
+			// Increment initial, indicating that the first two cards have been dealt to both
+			// dealer and player.
+			initial++;
+			System.out.println("Player score: " + score(player));
+			if(score(player) >= 21 || !hitDoubleStand){
+				dealerLogic();
+				checkState();
 			}
 		}
-		
+		System.out.println("Player score: " + score(player) +
+				   "\nDealer score: " + score(dealer));
+		if(state == 1){
+			money += bet;
+			System.out.println("You won $ " + bet);
+		}
+		else if(state == 2){
+			money -= bet;
+			System.out.println("Dealer wins! You lost $ " +bet);
+		}
+		else if(state == 3){
+			System.out.println("Push!");
+		}
 	}
 	/**
 	 * @param args
@@ -98,18 +151,26 @@ public class Game {
 	public void deal(int seed, boolean turn){
 		// Used to deal cards randomly
 		Random rand = new Random();
+		
 		// Keeps track of the random number, to get and remove card from deck.
-		int index = rand.nextInt(seed);
+		int index = 0;
+
+		// Prevents a non-positive random number from being generated. 
+		while(index <= 0){
+			index = rand.nextInt(seed);
+		}
 		
 		// Checks if it is the player's turn or the dealer's turn to be dealt a card.
 		// True means it's the player's turn and false means it's the dealers turn. 
-		if(turn)
-			player.add(deck.get(rand.nextInt(index)));
-		else 
-			dealer.add(deck.get(rand.nextInt(index)));
-		// Removes the cards from the deck of 52 as they are dealt to player/dealer
-		deck.remove(index);
+			if(turn)
+				player.add(deck.get(rand.nextInt(index)));
+			else 
+				dealer.add(deck.get(rand.nextInt(index)));
+			// Removes the cards from the deck of 52 as they are dealt to player/dealer
+			deck.remove(index);
+			size--;
 	}
+	
 	/**
 	 * Adds the value of the parameter cards together and returns the 
 	 * result. Accounts for aces, which can have a value of either 1 
@@ -148,5 +209,71 @@ public class Game {
 			return score + 1;
 		}
 		return score;
+	}
+	
+	/**
+	 * Changes the state of the game by checking to see what cards the
+	 * dealer and the player hold. A state equaling 0 means the game
+	 * has started/is in progress and there are no wins/loses/ties. A
+	 * score of 1 means the player wins, while a score of 2 means the
+	 * dealer wins. A score of 3 means it is a tie game. Also provides
+	 * checks for true blackjacks. True blackjack is when someone has
+	 * an Ace plus a 10 or a face card. 
+	 */
+	private void checkState(){
+		// Checks for when both the player and the dealer have a hand that is greater than
+		// or equal to 21. 
+		if(score(player) <= 21 && score(dealer) <= 21){
+			// Various checks for when the player and dealer have the same score
+			if(score(player) == score(dealer)){
+				
+				// A check for a true blackjack
+				if(score(player) == 21){
+					if(dealer.size() == 2 && player.size() == 2)
+						state = 3;
+					else if(dealer.size() == 2 && player.size() != 2)
+						state = 2;
+					// Pays the player 3:2 if they have true blackjack
+					else {
+						state = 1;
+						bet *= 1.5;
+					}
+				}
+				state = 3;
+			}
+			// Checks for when the player has a better hand than the dealer
+			else if(score(player) > score(dealer)){
+				
+				if(score(player) == 21){
+					// Pays the player 3:2 if they have true blackjack.
+					if(player.size() == 2)
+						bet *= 1.5;
+				state = 1;
+				}
+			}
+			// If the dealer has a better hand than the player, the dealer wins
+			else
+				state = 2;
+		}
+		// Checks for situations in which either the dealer or the player have a 
+		// score greater than 21. 
+		if(score(player) > 21 || score(dealer) > 21){
+			if(score(player) > 21 && score(dealer) > 21)
+				state = 2;
+			else if(score(player) > 21)
+				state = 2;
+			else 
+				state = 1;
+		}
+	}
+	/**
+	 * Determines whether the dealer will hit or
+	 * stand. Dealer stands at 17 in this game. 
+	 */
+	private void dealerLogic(){
+		if(score(dealer) < 17){
+			deal(size, false);
+			size--;
+		}
 	}
 }
